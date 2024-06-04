892125a5d564e4cbe0e3eb8ba47453d9df502c9f
Harsh Sharma <harshvs415@gmail.com>
2024-06-04 04:48:16 +0000

    [LeetCode Sync] Runtime - 55 ms (80.92%), Memory - 17.7 MB (30.20%)

---
+++ my-folder/0001-two-sum/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict1 = {}
+
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in dict1:
+                return [dict1[comp], i]
+            else:
+                dict1[nums[i]] = i        
