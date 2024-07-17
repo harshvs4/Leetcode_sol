f13f4e0f4fd1dda65989e0bcccaae3348c686aa8
Harsh Sharma <harshvs415@gmail.com>
2024-07-17 05:09:21 +0000

    [LeetCode Sync] Runtime - 162 ms (89.62%), Memory - 18 MB (80.23%)

--- my-folder/0169-majority-element/solution.py
+++ my-folder/0169-majority-element/solution.py
@@ -1,11 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dict1 = {}
-        for i in nums:
-            if i in dict1:
-                dict1[i] += 1
-            else:
-                dict1[i] = 1
+        candidate = None
+        count = 0
 
-        return max(dict1, key=dict1.get)
-        
+        for n in nums:
+            if count == 0:
+                candidate = n
+                count += 1
+            elif candidate == n:
+                count += 1
+            else:
+                count -= 1
+        return candidate
