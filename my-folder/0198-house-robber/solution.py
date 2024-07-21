fe4a9ea945b0e2cd9e71703e3ffb7097304fccf4
Harsh Sharma <harshvs415@gmail.com>
2024-07-21 02:40:36 +0000

    [LeetCode Sync] Runtime - 37 ms (48.07%), Memory - 16.4 MB (69.18%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        m = {}
-        def r(n):
-            if n < 0: 
-                return 0
-            if n in m:
-                return m[n]
-
-            result = max(nums[n] + r(n-2), r(n-1))
-            m[n] = result
-            return result
-        return r(len(nums)-1)
+        prev = nums[0]
+        prev2 = 0
+        for i in range(1, len(nums)):
+            notPick = 0 + prev
+            pick = nums[i] + prev2
+            curr = max(pick, notPick)
+            prev2 = prev
+            prev = curr
+        return prev
+        
