0afd2ca6796e40ac91c3cab0a2295aa5dafd24ee
Harsh Sharma <harshvs415@gmail.com>
2024-07-21 03:04:37 +0000

    [LeetCode Sync] Runtime - 44 ms (7.79%), Memory - 16.6 MB (32.84%)

--- my-folder/0213-house-robber-ii/solution.py
+++ my-folder/0213-house-robber-ii/solution.py
@@ -1,22 +1,18 @@
 class Solution:
+    def pick(self, nums: List[int]) -> int:
+        prev = nums[0]
+        prev2 = 0
+        for i in range(1, len(nums)):
+            notPick = 0 + prev
+            pick = nums[i] + prev2
+            curr = max(pick, notPick)
+            prev2 = prev
+            prev = curr
+        return prev
+
     def rob(self, nums: List[int]) -> int:
-        def rob2(nums):
-            m = {}
-            def r(n):
-                if n < 0:
-                    return 0 
-                if n in m:
-                    return m[n]
+        if len(nums) == 1:return nums[0]
+        x = nums[:-1]
+        y = nums[1:]
 
-                result= max(nums[n]+r(n-2), r(n-1))
-                m[n] = result
-                return result
-            return r(len(nums)-1)
-        if not nums:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
-
-        p = rob2(nums[1:])
-        q = rob2(nums[:-1])
-        return max(p,q)
+        return max(self.pick(x), self.pick(y))
