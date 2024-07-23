7c7e1871b796914b48e934cf499367a0a8ceb215
Harsh Sharma <harshvs415@gmail.com>
2024-07-23 02:12:50 +0000

    [LeetCode Sync] Runtime - 82 ms (82.40%), Memory - 17.1 MB (98.72%)

--- my-folder/0064-minimum-path-sum/solution.py
+++ my-folder/0064-minimum-path-sum/solution.py
@@ -1,21 +1,44 @@
 class Solution:
-    def countp(self, i, j, dp, grid):
-            if i == 0 and j == 0: return grid[0][0]
+    def path(self, i, j, grid, dp):
+        if i == 0 and j == 0:
+            return grid[i][j]
 
-            if i < 0 or j < 0: return float('inf')
+        if i < 0 or j < 0:
+            return float('inf')
 
-            if dp[i][j] != -1: return dp[i][j]
+        if dp[i][j] != -1:return dp[i][j]
 
-            up = grid[i][j] + self.countp(i-1, j, dp, grid)
-            
-            left = grid[i][j] + self.countp(i, j-1, dp, grid)
-            
-            dp[i][j] = min(up, left)
-            return dp[i][j]
+        up = grid[i][j] + self.path(i-1, j, grid, dp)
+        left = grid[i][j] + self.path(i, j-1, grid, dp)
+
+        dp[i][j] = min(up, left)
+
+        return dp[i][j]
 
     def minPathSum(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        dp = [[-1 for j in range(m)] for i in range(n)]
-        return self.countp(n-1, m-1, dp, grid)
+        prev = [0]*m
+
+        for i in range(n):
+            temp = [0]*m
+            for j in range(m):
+                if i == 0 and j == 0:
+                    temp[j] = grid[0][0]
+                else:
+                    up = grid[i][j]
+                    if i > 0:
+                        up += prev[j]
+                    else:
+                        up = float('inf')
+                    left = grid[i][j]
+                    if j > 0:
+                        left += temp[j-1]
+                    else:
+                        left = float('inf')
+                    
+                    temp[j] = min(up, left)
+            prev = temp
+
+        return prev[m-1]
         
