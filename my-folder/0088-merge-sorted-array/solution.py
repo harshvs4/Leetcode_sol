f5102fcbf9a3a5cdafd6e78cb6f7f88eb503dd0b
Harsh Sharma <harshvs415@gmail.com>
2024-08-05 00:19:19 +0000

    [LeetCode Sync] Runtime - 39 ms (59.90%), Memory - 16.6 MB (52.40%)

--- my-folder/0088-merge-sorted-array/solution.py
+++ my-folder/0088-merge-sorted-array/solution.py
@@ -3,15 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        a, b, write_index = m-1, n-1, m + n - 1
+        x = m+n-1
+        i = m-1
+        j = n-1
 
-        while b >= 0:
-            if a >= 0 and nums1[a] > nums2[b]:
-                nums1[write_index] = nums1[a]
-                a -= 1
+        while i >= 0 and j >= 0:
+            if nums2[j] >= nums1[i]:
+                nums1[x] = nums2[j]
+                j-=1
             else:
-                nums1[write_index] = nums2[b]
-                b -= 1
-
-            write_index -= 1
+                nums1[x] = nums1[i]
+                i-=1
+            x-=1
+        
+        while j >= 0:
+            nums1[x] = nums2[j]
+            x-=1
+            j-=1
         
