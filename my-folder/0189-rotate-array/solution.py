08d90d1ac832f4497ab856912362b9e20e9ca607
Harsh Sharma <harshvs415@gmail.com>
2024-08-06 13:07:05 +0000

    [LeetCode Sync] Runtime - 158 ms (57.70%), Memory - 27.9 MB (66.35%)

--- my-folder/0189-rotate-array/solution.py
+++ my-folder/0189-rotate-array/solution.py
@@ -1,22 +1,18 @@
 class Solution:
+    def lrotate(self, nums, start, end):
+        while start <= end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        k %= n
-        def swap(x,y,nums):
-            temp = nums[x]
-            nums[x] = nums[y]
-            nums[y] = temp
-        
-        def lrotate(start, end, nums):
-            while start <= end:
-                swap(start, end, nums)
-                start+=1
-                end-=1
-
-        lrotate(0,n-1, nums)
-        lrotate(0,k-1,nums)
-        lrotate(k, n-1, nums)
+        if n <= 1:
+            return
+        k = k%n
+        self.lrotate(nums, 0, n-1)
+        self.lrotate(nums, 0, k-1)
+        self.lrotate(nums, k, n-1)
         
