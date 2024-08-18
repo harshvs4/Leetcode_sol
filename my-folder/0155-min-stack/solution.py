38dabff2383ec06ffcc3f303330950bb757c9061
Harsh Sharma <harshvs415@gmail.com>
2024-08-18 23:59:04 +0000

    [LeetCode Sync] Runtime - 54 ms (60.93%), Memory - 20.4 MB (90.78%)

---
+++ my-folder/0155-min-stack/solution.py
@@ -0,0 +1,32 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        # Only push to min_stack if it's empty or the new value is <= current minimum
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        # If the value being popped is the current minimum, pop it from min_stack as well
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1] if self.stack else None
+        
+
+    def getMin(self) -> int:
+        return self.min_stack[-1] if self.min_stack else None
+        
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
