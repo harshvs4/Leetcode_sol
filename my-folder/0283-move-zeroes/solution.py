c5264330c2307f18c178303972ecb7f4c076eb15
Harsh Sharma <harshvs415@gmail.com>
2024-09-10 11:58:01 +0000

    [LeetCode Sync] Runtime - 120 ms (82.47%), Memory - 18.1 MB (21.94%)

--- my-folder/0283-move-zeroes/solution.py
+++ my-folder/0283-move-zeroes/solution.py
@@ -3,10 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                for j in range(i, len(nums)):
-                    if nums[j] != 0:
-                        nums[j], nums[i] = nums[i], nums[j]
-                        break
-        
+        n = len(nums)
+        j = 0  # This pointer will track the position to place the next non-zero element.
+
+        # Traverse through the list.
+        for i in range(n):
+            if nums[i] != 0:
+                # Swap the non-zero element with the element at index `j`.
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1  # Move `j` to the next position for non-zero elements.
+
